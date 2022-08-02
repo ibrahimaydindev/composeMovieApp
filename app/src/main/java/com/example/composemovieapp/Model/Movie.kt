7ab9c3be46e9440b21170514d85641e75e50c98f
ibrahim <aydinibrahim1692@gmail.com>
2022-08-02 16:04:13 +0300

    Updated app

---
+++ app/src/main/java/com/example/composemovieapp/Model/Movie.kt
@@ -0,0 +1,4 @@
+package com.example.composemovieapp.Model
+
+data class Movie (val name:String,val imageUrl:String,val desc:String,val category:String){
+}
---
+++ app/src/main/java/com/example/composemovieapp/viewModel/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.composemovieapp.viewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.composemovieapp.Api.ApiService
+import com.example.composemovieapp.Model.Movie
+import kotlinx.coroutines.launch
+
+class MainViewModel:ViewModel() {
+
+    var movieListResponse:List<Movie> by mutableStateOf(listOf())
+    var errorMessage: String by mutableStateOf("")
+    fun getMovieList() {
+        viewModelScope.launch {
+            val apiService = ApiService.getInstance()
+            try {
+                val movieList = apiService.getMovies()
+                movieListResponse = movieList
+            }
+            catch (e: Exception) {
+                errorMessage = e.message.toString()
+            }
+        }
+    }
+}
